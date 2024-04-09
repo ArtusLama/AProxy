a714ddc68a623be6e22547b8c855384641d926da
fledermaus101 <73125184+fledermaus101@users.noreply.github.com>
2024-04-09 22:18:15 +0200

    Continued working on MinecraftClient

--- src/main/java/de/artus/proxy/client/play/MinecraftClient.java
+++ src/main/java/de/artus/proxy/client/play/MinecraftClient.java
@@ -2,26 +2,47 @@ package de.artus.proxy.client.play;
 
 import de.artus.proxy.packets.fieldtypes.StringField;
 import de.artus.proxy.packets.fieldtypes.UShortField;
+import de.artus.proxy.packets.fieldtypes.UUIDField;
 import de.artus.proxy.packets.fieldtypes.VarIntField;
 import de.artus.proxy.packets.listener.PacketManager;
+import de.artus.proxy.packets.listener.client.ClientPacketStreamListener;
+import de.artus.proxy.packets.packet.PacketState;
 import de.artus.proxy.packets.packet.c2s.C2SHandshake;
+import de.artus.proxy.packets.packet.c2s.C2SLoginStart;
+import de.artus.proxy.packets.packet.s2c.S2CDisconnect;
+import de.artus.proxy.packets.packet.s2c.S2CEncryptionRequest;
+import de.artus.proxy.util.SRVLookup;
 import de.artus.proxy.util.ServerAddress;
+import de.artus.proxy.util.Version;
+import de.artus.proxy.util.player.Player;
 import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
+@Getter
+@RequiredArgsConstructor
+@Slf4j
 public class MinecraftClient {
 
-    @Getter @Setter
-    private boolean connected;
-    @Getter @Setter
+    @Setter
+    private boolean connected = false;
+    @Setter
     private Socket connection;
 
+    @Getter @Setter @NonNull
+    private Player player;
 
-    @Getter
+    @Getter @Setter @NonNull
+    private Version version;
+
+
     private PacketManager packetManager = new PacketManager();
 
 
@@ -30,18 +51,30 @@ public class MinecraftClient {
 
         setConnection(new Socket());
         DataOutputStream outputStream = new DataOutputStream(getConnection().getOutputStream());
-
+        ClientPacketStreamListener packetListener = new ClientPacketStreamListener(
+                new DataInputStream(getConnection().getInputStream()),
+                getPacketManager()
+        );
 
         // Step 1: Send handshake packet
+        packetListener.listen(PacketState.LOGIN); // because HANDSHAKE packet can only be received as SERVER so using LOGIN state
         getPacketManager().sendPacket(new C2SHandshake(
-                new VarIntField(-1), // not planning to log in, so the version doesn't matter
+                new VarIntField(getVersion().getProtocol()), // not planning to log in, so the version doesn't matter
                 new StringField(server.getHost()),
                 new UShortField(server.getPort()),
                 new VarIntField(2) // status = 2 to indicate that we want to log in
         ), outputStream);
 
         // Step 2: Send login start packet
-        // ...
+        getPacketManager().sendPacket(new C2SLoginStart(
+                new StringField(getPlayer().getName()),
+                new UUIDField(getPlayer().getUuid())
+        ), outputStream);
+
+        // Step 3: Listen for encryption request
+        S2CEncryptionRequest encryptionRequest = packetListener.expectPacket(S2CEncryptionRequest.class);
+        log.info(new GsonBuilder().setPrettyPrinting().create().toJson(encryptionRequest));
+
 
     }
 
