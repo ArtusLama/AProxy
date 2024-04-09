c1c14354732ab81f7f43fbaaab95b352b91b4814
fledermaus101 <73125184+fledermaus101@users.noreply.github.com>
2024-04-09 22:16:56 +0200

    Added Logging

--- src/main/java/de/artus/proxy/packets/fieldtypes/StringField.java
+++ src/main/java/de/artus/proxy/packets/fieldtypes/StringField.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.Accessors;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -12,6 +13,7 @@ import java.io.IOException;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Slf4j
 public class StringField implements FieldType<String> {
 
     @Accessors(chain = true)
@@ -22,13 +24,16 @@ public class StringField implements FieldType<String> {
     @Override
     public StringField read(DataInputStream stream) throws IOException {
         int length = new VarIntField().read(stream).getValue();
+        log.trace("Reading String with len: {}", length);
         setValue(new String(stream.readNBytes(length)));
+        log.trace("Read String \"{}\"", getValue());
 
         return this;
     }
 
     @Override
     public StringField write(DataOutputStream stream) throws IOException {
+        log.trace("Writing String ({})\n\"{}\"", getValue().getBytes().length, getValue());
         new VarIntField().setValue(getValue().getBytes().length).write(stream);
         stream.write(getValue().getBytes());
 
